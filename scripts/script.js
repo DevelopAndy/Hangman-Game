963eb58370f0d30ce68cbaa7a0a4458734207837
Andy <ander1386@gmail.com>
2022-06-30 14:59:01 -0500

    creating word arrays and function for random words

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
-//traemos el canvas para dibujar
+//bringing the canvas to draw
 const canvas = document.querySelector('canvas'), 
       context = canvas.getContext('2d');
 
-//función dibujar lineas
+//draw lines
 function draw(xi,yi,xf,yf) 
 {
     context.beginPath();
@@ -13,19 +13,49 @@ function draw(xi,yi,xf,yf)
 }
 
 draw(200,199,400,199); //base
-draw(230,200,230,0); //poste
-draw(230,1,360,1); //techo
-draw(360,0,360,30); //cuerda
-draw(360,70,360,130); //cuerpo
-draw(360,130,380,170); //pierna derecha
-draw(360,130,340,170); //pierna derecha
-draw(360,80,380,120); //brazo derecho
-draw(360,80,340,120); //brazo izquierdo
+draw(230,200,230,0); //pole
+draw(230,1,360,1); //ceiling
+draw(360,0,360,30); //rope
+draw(360,70,360,130); //body
+draw(360,130,380,170); //right leg
+draw(360,130,340,170); //left leg
+draw(360,80,380,120); //right arm
+draw(360,80,340,120); //left arm
 
-//dibujar cabeza
+//draw head
 context.beginPath();
 context.arc(360,50,20,0,2*Math.PI);
 context.stroke();
+
+//---------------------- beginning code for words --------------------
+
+const words = ["caballo", "perro", "gato", "manzana", "pera", "desayuno", "huevo", "cocina", "doctor", "verano", "invierno"];
+
+function randWord(array)
+{
+    let rand = Math.random()*array.length | 0;
+    let rValue = array[rand];
+    return rValue;
+}
+
+// code to not repeat the words
+
+// wordGame = [];
+
+// for (let i in words)
+// { 
+//     newWord = randWord(words);
+//     while (wordGame.includes(newWord))
+//     {
+//         newWord = randWord(words);
+//     }
+    
+//     wordGame.push(newWord);
+
+//     console.log(firstWord);
+// }
+
+console.log(randWord(words));
 
 
 
